c9c669d88da9865e8c889dbf6a38217c254c69dc
samgashyeong <samgashyeong@naver.com>
2021-08-31 00:50:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/databindingproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.databindingproject
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.databindingproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
